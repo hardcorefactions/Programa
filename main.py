8a1a69c44ccf363041f5601500e511f6be028cea
TheBraisFP <brais.cambados@gmail.com>
2022-12-20 17:24:43 +0100

    Another commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from colorama import Fore
 import os
+import time
+
 username = input("Username: ")
 url = f'https://api.mojang.com/users/profiles/minecraft/{username}?'
 response = requests.get(url)
@@ -13,5 +15,7 @@ print("")
 print(Fore.YELLOW + "UUID".center(50, " "))
 print(Fore.WHITE + uuid.center(50, " "))
 print(Fore.WHITE + "")
-
+time.sleep(2)
+print(Fore.CYAN + "Made by xZexius#0001".center(50, " "))
+print(Fore.WHITE + "")
 
